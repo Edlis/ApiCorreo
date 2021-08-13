42f5552659f65c935f7f7482c64c12a10f2d9397
Edlis <edna.p.m.2008@gmail.com>
2021-08-12 23:51:52 -0600

    Add project files.

---
+++ Procesos/Consumidor.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Mensajes;
+using Modelos.Conn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procesos
+{
+    public class Consumidor : IConsumer<Correo>
+    {
+        public readonly SMTPConfiguracionModelo _smtp;
+        public Consumidor(SMTPConfiguracionModelo smtp) {
+            _smtp = smtp;
+        }
+
+        public Task Consume(ConsumeContext<Correo> context)
+        {
+            var correo = context.Message;
+            clsCorreo email = new clsCorreo(_smtp);
+            email.Enviar(correo);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Procesos/clsCorreo.cs
@@ -0,0 +1,50 @@
+﻿using Mensajes;
+using Modelos.Conn;
+using System;
+using System.Net.Mail;
+
+namespace Procesos
+{
+    public class clsCorreo
+    {
+        public SMTPConfiguracionModelo _smtp;
+
+        public clsCorreo(SMTPConfiguracionModelo smtp)
+        {
+            this._smtp = smtp;
+        }
+
+
+        public string Enviar(Correo correo)
+        {            
+            try
+            {                
+                MailMessage message = new MailMessage(_smtp.Email, correo.Destinatario);
+                message.Subject = correo.Titulo;
+                message.Body = correo.Mensaje;
+                SmtpClient client = new SmtpClient(_smtp.Host, _smtp.Port);
+                //se le quito seguridad al correo
+                client.UseDefaultCredentials = false;
+                client.EnableSsl = true;
+                client.Credentials = new System.Net.NetworkCredential(_smtp.Email, _smtp.Password);
+
+                try
+                {
+                    client.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ocurrio un error al enviar el correo: {0}",
+                        ex.ToString());
+                    throw ex;
+                }
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
